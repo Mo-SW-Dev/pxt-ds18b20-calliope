8bd9705343feb629c3993fa8e4f48da131813ed0
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2018-12-23 20:22:07 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 // tests go here; this will not be compiled when this package is used as a library
 
-basic.forever(() => {
-    serial.writeValue("0", DS18B20.TemperatureNumber(DS18B20.pin.P0))
-    serial.writeLine("0 : " + DS18B20.TemperatureString(DS18B20.pin.P0))
-    basic.pause(1000)
-})
+//basic.forever(() => {
+//    serial.writeValue("0", DS18B20.TemperatureNumber(DS18B20.pin.P0))
+ //   serial.writeLine("0 : " + DS18B20.TemperatureString(DS18B20.pin.P0))
+ //   basic.pause(1000)
+//})
