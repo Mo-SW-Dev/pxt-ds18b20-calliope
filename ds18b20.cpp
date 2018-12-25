ec0f26ed095b60d31cc65c4d216f8ddcbc4b9a20
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2018-12-25 22:26:56 +0100

    Update ds18b20.cpp

--- ds18b20.cpp
+++ ds18b20.cpp
@@ -70,18 +70,18 @@ class microbitp : public MicroBitComponent
 
 //    MicroBit uBit;
  //   MicroBitPin pin1 = uBit.io.P2;
-//    microbitp  pin0(7, 3, 15);
-    microbitp  pin0(7, MICROBIT_PIN_P0, PIN_CAPABILITY_ALL);
-    microbitp  pin1(8, MICROBIT_PIN_P1, PIN_CAPABILITY_ALL);
-    microbitp  pin2(9, MICROBIT_PIN_P2, PIN_CAPABILITY_ALL);
-    microbitp  pin5(12, MICROBIT_PIN_P5, PIN_CAPABILITY_STANDARD);        //BTN_A
-    microbitp  pin8(15, MICROBIT_PIN_P8, PIN_CAPABILITY_STANDARD);        //PIN 18
-    microbitp  pin11(18,MICROBIT_PIN_P11,PIN_CAPABILITY_STANDARD);        //BTN_B
-    microbitp  pin12(19,MICROBIT_PIN_P12,PIN_CAPABILITY_STANDARD);        //PIN 20
-    microbitp  pin13(20,MICROBIT_PIN_P13,PIN_CAPABILITY_STANDARD);        //SCK
-    microbitp  pin14(21,MICROBIT_PIN_P14,PIN_CAPABILITY_STANDARD);        //MISO
-    microbitp  pin15(22,MICROBIT_PIN_P15,PIN_CAPABILITY_STANDARD);        //MOSI
-    microbitp  pin16(23,MICROBIT_PIN_P16,PIN_CAPABILITY_STANDARD);        //PIN 16
+//    microbitp  pin0(7, 3, 15);                                          // Calliope Pins
+    microbitp  pin0(7, MICROBIT_PIN_P0, PIN_CAPABILITY_ALL);              //P1
+    microbitp  pin1(8, MICROBIT_PIN_P1, PIN_CAPABILITY_ALL);              //P2
+    microbitp  pin2(9, MICROBIT_PIN_P2, PIN_CAPABILITY_ALL);              //C16
+ //   microbitp  pin5(12, MICROBIT_PIN_P5, PIN_CAPABILITY_STANDARD);        //
+    microbitp  pin8(15, MICROBIT_PIN_P8, PIN_CAPABILITY_STANDARD);        //C17
+ //   microbitp  pin11(18,MICROBIT_PIN_P11,PIN_CAPABILITY_STANDARD);        //
+    microbitp  pin12(19,MICROBIT_PIN_P12,PIN_CAPABILITY_STANDARD);        //P0
+ //   microbitp  pin13(20,MICROBIT_PIN_P13,PIN_CAPABILITY_STANDARD);        //
+  //  microbitp  pin14(21,MICROBIT_PIN_P14,PIN_CAPABILITY_STANDARD);        //
+ //   microbitp  pin15(22,MICROBIT_PIN_P15,PIN_CAPABILITY_STANDARD);        //
+    microbitp  pin16(23,MICROBIT_PIN_P16,PIN_CAPABILITY_ALL);              //P3
     
     microbitp  pin = pin0;
 
@@ -161,13 +161,13 @@ class microbitp : public MicroBitComponent
           case 0: pin = pin0; break;
           case 1: pin = pin1; break;
           case 2: pin = pin2; break;
-          case 5: pin = pin5; break;
+    //      case 5: pin = pin5; break;
           case 8: pin = pin8; break;
-          case 11: pin = pin11; break;
+     //     case 11: pin = pin11; break;
           case 12: pin = pin12; break;
-          case 13: pin = pin13; break;
-          case 14: pin = pin14; break;
-          case 15: pin = pin15; break;
+      //    case 13: pin = pin13; break;
+      //    case 14: pin = pin14; break;
+      //    case 15: pin = pin15; break;
           case 16: pin = pin16; break;
           default: pin = pin0;
         }
